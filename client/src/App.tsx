5a65b5bbebd872d90563ae0a5cd7e1e3ec9ec24d
Keerthik Kalyan <info@centuaryfab.com>
2026-03-26 12:41:05 +0530

    commit changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,5 @@
-import { Switch, Route } from "wouter";
-import Home from "@/pages/Home";
 import Products from "@/pages/Products";
 
 export default function App() {
-  return (
-    <div>
-      <Switch>
-        <Route path="/" component={Home} />
-        <Route path="/products" component={Products} />
-      </Switch>
-    </div>
-  );
+  return <Products />;
 }
